9cb435dc0e9e0273025d933ce6693c8049e1f961
chb.wang <chb.wang@hnair.com>
2021-08-25 16:43:41 +0800

    docs: 编辑路径更改

--- packages/nidle-site/docusaurus.config.js
+++ packages/nidle-site/docusaurus.config.js
@@ -98,13 +98,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/yanuoda/nidle/edit/main/packages/nidle-site/'
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/yanuoda/nidle/edit/main/packages/nidle-site/blog/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
