7780044231dc6ac9d8e9693d2fb87d5c31951368
chb.wang <chb.wang@hnair.com>
2021-09-03 13:46:08 +0800

    style: 参数检查优化

--- packages/nidle-core/lib/log/logger.js
+++ packages/nidle-core/lib/log/logger.js
@@ -5,12 +5,17 @@ const transport = require('./transport')
 
 module.exports = function (options) {
   const { destination } = options
+
+  if (!destination) {
+    throw new Error('options.destination must required!')
+  }
+
   const transports = transport({
     worker: options.worker || {},
     destination: {
       all: path.resolve(destination, 'all.log'),
-      error: path.resolve(destination, 'error.log'),
-      pretty: path.resolve(destination, 'pretty.log')
+      error: path.resolve(destination, 'error.log')
+      // pretty: path.resolve(destination, 'pretty.log')
     }
   })
 
--- packages/nidle-core/lib/log/transport.js
+++ packages/nidle-core/lib/log/transport.js
@@ -1,32 +1,26 @@
 const pino = require('pino')
 
 module.exports = (options) => {
+  const targets = [
+    // 错误日志
+    {
+      level: 'error',
+      target: '#pino/file',
+      options: {
+        destination: options.destination.error
+      }
+    },
+    // 所有日志
+    {
+      target: '#pino/file',
+      options: {
+        destination: options.destination.all
+      }
+    }
+  ]
+
   return pino.transport({
-    targets: [
-      // 错误日志
-      {
-        level: 'error',
-        target: '#pino/file',
-        options: {
-          destination: options.destination.error
-        }
-      },
-      // 所有日志
-      {
-        target: '#pino/file',
-        options: {
-          destination: options.destination.all
-        }
-      },
-      // 格式化后的日志
-      // {
-      //   target: '#pino/pretty',
-      //   options: {
-      //     destination: options.destination.pretty
-      //   }
-      // }
-      // TODO: socket
-    ],
+    targets,
     worker: options.worker
   })
 }
