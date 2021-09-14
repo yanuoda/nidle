66bd36a745c4daf2889916bbe3586010a69d8350
chb.wang <chb.wang@hnair.com>
2021-09-14 17:34:03 +0800

    feat: 备份功能

---
+++ packages/nidle-core/lib/backup/backup.js
@@ -0,0 +1,78 @@
+import p from 'path'
+import fs from 'fs'
+import { copy, compress, decompress } from './util'
+const cFileExtname = '.tgz'
+
+class Backup {
+  // options = { name: '', backup: {}, cache: {}, path }
+  constructor (options) {
+    this.options = {
+      ...options
+    }
+  }
+
+  /**
+   * 备份 - 只用于生产环境
+   * 从缓存备份
+   * @returns Promise
+   */
+  backup () {
+    const { name, backup, cache, path } = this.options
+    const basename = p.basename(path)
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        await copy(`${p.resolve(cache.path, basename)}${cFileExtname}`, backup.path, true)
+
+        // 备份数量超过限制将删除老文件
+        const files = fs.readdirSync(backup.path)
+        const backups = files.filter(item => item.indexOf(name) === 0)
+
+        if (backups.length > backup.maxCount) {
+          const rmBackup = backups.sort().shift()
+          fs.rmSync(p.resolve(backup.path, rmBackup))
+        }
+
+        resolve()
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  /**
+   * 回滚 - 只用于生产环境
+   * 只负责将备份还原到目标目录
+   * @returns Promise
+   */
+  rollback () {
+    const { backup, path } = this.options
+    const basename = p.basename(path)
+
+    return decompress(`${p.resolve(backup.path, basename)}${cFileExtname}`, path, false)
+  }
+
+  /**
+   * 缓存 - 缓存阶段、非生产环境集成，用于重试、下一环境集成
+   * @param {Boolean} isRemove 是否移除源文件，只有最后一个阶段才移除源文件
+   * @returns Promise
+   */
+  cache (isRemove = false) {
+    const { cache, path } = this.options
+
+    return compress(path, cache.path, isRemove, cFileExtname)
+  }
+
+  /**
+   * 还原 - 重试时还原上阶段集成，下一环境集成时还原上一环境集成
+   * @returns Promise
+   */
+  restore () {
+    const { cache, path } = this.options
+    const basename = p.basename(path)
+
+    return decompress(`${p.resolve(cache.path, basename)}${cFileExtname}`, path, false)
+  }
+}
+
+export default Backup
---
+++ packages/nidle-core/lib/backup/util.js
@@ -0,0 +1,82 @@
+import path from 'path'
+import execa from 'execa'
+
+/**
+ * 压缩input目录，并且以目录名命名放到output目录
+ * 覆盖目标文件
+ * @param {String} input 要压缩的目录
+ * @param {String} output 输出目标目录
+ * @param {Boolean} isRemove 是否移除原文件
+ * @param {String} extname 扩展名
+ * @returns Promise
+ */
+export const compress = function (input, output = '.', isRemove = false, extname = '.tgz') {
+  return new Promise((resolve, reject) => {
+    const basename = path.basename(input)
+    let shell = `cd ${path.dirname(input)} && tar -zcpf ${output}/${basename}${extname} ./${basename}`
+
+    if (isRemove) {
+      shell += ` && rm -rf ${input}`
+    }
+
+    execa(shell, {
+      shell: true
+    }).then(() => {
+      resolve()
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
+/**
+ * 解压input目录，放到output目录
+ * 覆盖目标文件
+ * @param {String} input 要解缩的目录
+ * @param {String} output 输出目标目录
+ * @param {Boolean} isRemove 是否移除原文件
+ * @returns Promise
+ */
+export const decompress = function (input, output = '.', isRemove = false) {
+  return new Promise((resolve, reject) => {
+    let shell = `tar -zxpf ${input} -C ${path.dirname(output)}`
+
+    if (isRemove) {
+      shell += ` && rm -rf ${input}`
+    }
+
+    execa(shell, {
+      shell: true
+    }).then(() => {
+      resolve()
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
+/**
+ * 拷贝input目录，并且以目录名命名放到output目录
+ * 覆盖目标文件
+ * @param {String} input 要拷贝的目录
+ * @param {String} output 输出目标目录
+ * @param {Boolean} isRemove 是否移除原文件
+ * @returns Promise
+ */
+export const copy = function (input, output = '.', isRemove = false) {
+  return new Promise((resolve, reject) => {
+    let shell = `cp -rpf ${input} ${output}`
+
+    if (isRemove) {
+      shell += ` && rm -rf ${input}`
+    }
+
+    execa(shell, {
+      shell: true
+    }).then(() => {
+      resolve()
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
