7625df295ce830a60671f1ed036b93e847cb99be
chb.wang <chb.wang@hnair.com>
2021-09-17 09:26:01 +0800

    refactor: 改为Logger类，worker.autoEnd set false, 任务结束时去手动刷日志

--- packages/nidle-core/lib/log/logger.js
+++ packages/nidle-core/lib/log/logger.js
@@ -2,38 +2,45 @@ import path from 'path'
 import pino from 'pino'
 import redact from './redaction.js'
 import transport from './transport.js'
+import { mkdir } from '../backup/util'
 
-export default function (options) {
-  const { destination } = options
+export default class Logger {
+  constructor (options) {
+    const { destination } = options
 
-  if (!destination) {
-    throw new Error('options.destination must required!')
+    if (!destination) {
+      throw new Error('options.destination must required!')
+    }
+
+    mkdir(destination)
+
+    const transports = transport({
+      worker: {
+        autoEnd: false
+      },
+      destination: {
+        all: path.resolve(destination, 'all.log'),
+        error: path.resolve(destination, 'error.log')
+        // pretty: path.resolve(destination, 'pretty.log')
+      }
+    })
+
+    this.logger = pino({
+      redact,
+      // base: undefined
+      // formatters: {
+      //   level (label, number) {
+      //     return {
+      //       level: number,
+      //       levelLabel: label
+      //     }
+      //   }
+      // }
+    }, transports)
+    this.transport = transports
   }
 
-  const transports = transport({
-    worker: options.worker || {},
-    destination: {
-      all: path.resolve(destination, 'all.log'),
-      error: path.resolve(destination, 'error.log')
-      // pretty: path.resolve(destination, 'pretty.log')
-    }
-  })
-
-  const logger = pino({
-    redact,
-    // base: undefined
-    // formatters: {
-    //   level (label, number) {
-    //     return {
-    //       level: number,
-    //       levelLabel: label
-    //     }
-    //   }
-    // }
-  }, transports)
-
-  return {
-    logger,
-    transport: transports
+  end () {
+    this.transport.end()
   }
 }
