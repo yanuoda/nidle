8ba6f1f65118b1d69785b6aac327522b9714e94f
chb.wang <chb.wang@hnair.com>
2021-09-23 15:34:23 +0800

    feat: 调度器类

--- packages/nidle-core/lib/manager.js
+++ packages/nidle-core/lib/manager.js
@@ -1,83 +1,145 @@
+import path, { resolve } from 'path'
+import fs from 'fs'
 import EventEmitter from 'eventemitter3'
 import { check, input, combine } from './config/index.js'
 import Scheduler from './scheduler/scheduler.js'
-import logger from './log/logger.js'
+import Logger from './log/logger.js'
 import Backup from './backup/backup.js'
 
 // 任务管理器
 class Manager extends EventEmitter {
   constructor (config) {
-    this.config = config
-    this.scheduler = null
-  }
+    super()
 
-  // 初始化
-  init () {
-    const { config } = this
     // 检查配置
     const { valid, message } = check(config)
 
     if (!valid) {
       throw new Error(message)
     }
+  
+    this.config = config
+    this.update = config.update
+    this.scheduler = null
+  }
+
+  // 初始化
+  async init () {
+    const { config } = this
 
     // 获取inputs
-    const inputs = input(config.stages)
+    try {
+      const inputs = await input(config.stages)
 
-    return {
-      inputs
+      return {
+        inputs
+      }
+    } catch (error) {
+      throw error
     }
   }
 
   // 挂载 - 用户确认input后
   mount (inputs) {
-    const { config } = this
+    return new Promise(resolve => {
+      const { config, update } = this
+      const basename = path.basename(config.output.path)
 
-    const log = this.logger = logger({
-      destination: config.log.path
-    })
-    const backup = this.backup = new Backup({
-      name: config.name,
-      ...config.output
-    })
-    const stages = combine(config.stages, inputs)
-    this.scheduler = new Scheduler(this, stages)
-    this.scheduler.mount()
-
-    this.scheduler.on('completed', () => {
-      // TODO: 状态
-
-      log.info({
-        progress: 'SCHEDULER COMPLETE'
+      const log = this.log = new Logger({
+        destination: config.log.path,
+        name: basename
       })
-      
-      try {
-        await backup.backup()
-      } catch (error) {
-        log.error({
-          progress: 'BACKUP ERROR',
-          error
+      log.transport.on('ready', () => {
+        resolve()
+      })
+      const logger = this.logger = log.logger
+      const backup = this.backup = new Backup({
+        name: config.name,
+        ...config.output
+      })
+      const stages = combine(config.stages, inputs)
+      const scheduler = this.scheduler = new Scheduler({
+        name: config.name,
+        repository: config.repository,
+        type: config.type,
+        output: {
+          path: config.output.path
+        },
+        logger
+      }, stages)
+      scheduler.mount()
+
+      scheduler.on('completed', async () => {
+        update({
+          status: 'success',
+          stage: ''
         })
-      }
 
-      this.emit('completed')
-    })
+        logger.info({
+          progress: 'SCHEDULER COMPLETE'
+        })
+        
+        try {
+          await backup.backup()
+          fs.rmSync(config.output.path, {
+            recursive: true
+          })
+        } catch (error) {
+          logger.error({
+            progress: 'BACKUP ERROR',
+            error
+          })
+        }
 
-    this.scheduler.on('error', error => {
-      this.emit('error', error)
+        log.end()
+        this.emit('completed')
+      })
+
+      scheduler.on('error', error => {
+        update({
+          status: 'error'
+        })
+
+        log.end()
+        this.emit('error', error)
+      })
+
+      scheduler.on('stage.active', name => {
+        // 记录运行到该stage
+        update({
+          stage: name
+        })
+      })
+
+      scheduler.on('stage.completed', async () => {
+        // 缓存
+        try {
+          await backup.cache()
+        } catch (error) {
+          logger.error({
+            progress: 'CACHE ERROR',
+            error
+          })
+        }
+      })
     })
   }
 
   // 构建开始
-  start (index = 0) {
-    const { logger: log } = this
+  async start (index = 0) {
+    const { logger } = this
 
     if (index !== 0) {
       // 重试，需要从缓存中恢复
       try {
-        this.backup.restore()
+        await this.backup.restore()
+
+        logger.info({
+          progress: 'RESTORE COMPLETE',
+          detail: '已从缓存还原代码'
+        })
       } catch (error) {
-        log.error({
+        logger.error({
           progress: 'RESTORE ERROR',
           error
         })
@@ -85,7 +147,7 @@ class Manager extends EventEmitter {
       }
     }
 
-    log.info({
+    logger.info({
       progress: 'SCHEDULER START',
       start: index
     })
@@ -93,8 +155,26 @@ class Manager extends EventEmitter {
   }
 
   // 回滚
-  rollback () {
+  async rollback () {
+    // 从备份中恢复，然后开始
+    const { logger, backup } = this
+    
+    try {
+      await backup.rollback()
 
+      logger.info({
+        progress: 'ROLLBACK COMPLETE',
+        detail: '备份已还原，开始回滚'
+      })
+
+      this.start()
+    } catch (error) {
+      logger.error({
+        progress: 'ROLLBACK ERROR',
+        error
+      })
+      throw new Error('备份还原失败，请重新开始分支发布')
+    }
   }
 }
 
---
+++ packages/nidle-core/test/fixtures/plugin-retry.js
@@ -0,0 +1,13 @@
+export default class {
+  apply (scheduler) {
+    scheduler.add('name', (task, config) => {
+      return new Promise((resolve, reject) => {
+        if (config.development === 'PRODUCTION') {
+          resolve()
+        } else {
+          reject(new Error(`can‘t use in ${config.development} development`))
+        }
+      })
+    })
+  }
+}
