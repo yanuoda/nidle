ef6d2395f8bccb2366e981d366cc96ccbd6b2d66
KlausChan03 <KlausChan03@gmail.com>
2022-01-04 17:11:43 +0800

    feat: 整合服务器管理模块，把设置由页面改为弹窗

--- packages/nidle-spa/src/pages/Server/List/index.jsx
+++ packages/nidle-spa/src/pages/Server/List/index.jsx
@@ -1,19 +1,117 @@
 import { PlusOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
+import { Tabs, Space, Modal, message, Button } from 'antd'
 import ProCard from '@ant-design/pro-card'
 import ProTable from '@ant-design/pro-table'
+import ProForm, { ProFormText } from '@ant-design/pro-form'
 import { PageContainer } from '@ant-design/pro-layout'
-import { Tabs, Space, Modal, message } from 'antd'
 import { ExclamationCircleOutlined } from '@ant-design/icons'
-import { useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Link, useModel } from 'umi'
-import { queryServerListByPage, deleteServer } from '@/services/server'
+import { queryServerListByPage, queryServerDetail, addServer, modifyServer, deleteServer } from '@/services/server'
+
+const ServerForm = props => {
+  const { id, environment } = props
+  console.log(id, environment)
+  // 应用服务器
+  const [server, setServer] = useState({})
+
+  useEffect(async () => {
+    // 获取服务器详情数据
+
+    if (id) {
+      const res = await queryServerDetail({ id })
+      const { success, data } = res || {}
+      if (success) {
+        setServer(data)
+      }
+    }
+  }, [])
+
+  /* 基本信息 */
+  const { name, ip, username, password, description } = server
+  console.log(server)
+  const ServerInfo = (
+    <ProForm
+      layout="vertical"
+      submitter={{
+        searchConfig: {
+          submitText: '保存'
+        }
+      }}
+      onFinish={async values => {
+        values = { ...values, environment }
+        const params = server.id ? { id: server.id, ...values } : values
+        const result = server.id ? await modifyServer(params) : await addServer(params)
+        const { success } = result || {}
+        if (success) {
+          props.onRefresh()
+          message.success('服务器信息保存成功，正在刷新页面...')
+        }
+      }}
+      initialValues={{ name, ip, username, password, description, environment }}
+    >
+      <ProFormText
+        width="xl"
+        name="name"
+        label=""
+        placeholder="请输入服务器名称"
+        required
+        rules={[{ required: true, message: '请输入服务器名称' }]}
+      />
+      <ProFormText
+        width="xl"
+        name="ip"
+        label=""
+        placeholder="请输入IP地址"
+        required
+        rules={[{ required: true, message: '请输入IP地址' }]}
+      />
+      <ProFormText
+        width="xl"
+        name="username"
+        label=""
+        placeholder="请输入用户名"
+        required
+        rules={[{ required: true, message: '请输入用户名' }]}
+      />
+      <ProFormText.Password
+        width="xl"
+        name="password"
+        type="password"
+        label=""
+        placeholder="请输入密码"
+        required
+        rules={[{ required: true, message: '请输入密码' }]}
+      />
+    </ProForm>
+  )
+
+  return <ProCard type="inner">{ServerInfo}</ProCard>
+}
 
 const ServerList = () => {
   // 获取环境配置信息
   const { initialState } = useModel('@@initialState')
   const { environmentList } = initialState || { environmentList: [] }
   const ref = useRef(null)
+  const [serverId, setServerId] = useState('')
+  const [serverEnv, setServerEnv] = useState('')
+  const [visible, setVisible] = useState(false)
+
+  const handleOpenModal = (env, id) => {
+    setVisible(true)
+    env && setServerEnv(env)
+    id && setServerId(id)
+  }
+
+  const handleCancel = () => {
+    setVisible(false)
+  }
+
+  const handleRefresh = () => {
+    handleCancel()
+    ref.current.reload()
+  }
 
   const handleDeleteServer = params => {
     Modal.confirm({
@@ -35,6 +133,7 @@ const ServerList = () => {
       message.success('删除成功！')
     }
   }
+
   const columns = [
     {
       title: '机器名',
@@ -51,12 +150,7 @@ const ServerList = () => {
       key: 'action',
       render: (text, record) => (
         <Space size="middle">
-          <Link
-            key="newServer"
-            to={`/server/form?id=${record.id}&name=${record.name}&environment=${record.environment}`}
-          >
-            编辑
-          </Link>
+          <a onClick={() => handleOpenModal(record.environment, record.id)}>编辑</a>
           <a onClick={() => handleDeleteServer(record)}>删除</a>
         </Space>
       )
@@ -78,11 +172,14 @@ const ServerList = () => {
                   pagination={{ pageSize: 10 }}
                   actionRef={ref}
                   toolBarRender={() => [
-                    <Link key="newServer" to={`/server/form?environment=${env.key}`}>
-                      <Button key="button" icon={<PlusOutlined />} type="primary">
-                        新建
-                      </Button>
-                    </Link>
+                    <Button
+                      key="button"
+                      icon={<PlusOutlined />}
+                      type="primary"
+                      onClick={() => handleOpenModal(env.key)}
+                    >
+                      新建
+                    </Button>
                   ]}
                 />
               </Tabs.TabPane>
@@ -90,11 +187,14 @@ const ServerList = () => {
           </Tabs>
         </>
       )}
+      <Modal visible={visible} footer={null} onCancel={() => handleCancel()}>
+        <ServerForm environment={serverEnv} id={serverId} onRefresh={() => handleRefresh()} />
+      </Modal>
     </ProCard>
   )
 }
 
-const ServerListContainer = props => {
+const ServerContainer = props => {
   const { name: serverName } = props.location.query
   // 面包屑导航自定义
   const routes = [
@@ -133,4 +233,4 @@ const ServerListContainer = props => {
   )
 }
 
-export default ServerListContainer
+export default ServerContainer
