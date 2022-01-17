d57cc79a7bf9fcb08b1ade7837927d16643bd139
okbeng03 <okbeng03@live.com>
2022-01-17 17:43:26 +0800

    fix: input

--- packages/nidle-plugin-merge/index.js
+++ packages/nidle-plugin-merge/index.js
@@ -19,6 +19,41 @@ class MergePlugin {
       })
     })
   }
+
+  input() {
+    return [
+      {
+        type: 'input',
+        name: 'apiUrl',
+        message: 'Git API base url'
+      },
+      {
+        type: 'input',
+        name: 'privateToken',
+        message: 'git privateToken'
+      },
+      {
+        type: 'input',
+        name: 'targetBranch',
+        message: 'merge request target branch'
+      },
+      {
+        type: 'confirm',
+        name: 'codeReview',
+        message: '是否需要codeReview'
+      },
+      {
+        type: 'confirm',
+        name: 'autoMerge',
+        message: '是否自动合并'
+      },
+      {
+        type: 'confirm',
+        name: 'removeSourceBranch',
+        message: '是否需要移除远程分支'
+      }
+    ]
+  }
 }
 
 export default MergePlugin
