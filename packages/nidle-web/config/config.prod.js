ad03e563564679cd2e20fbbf5f5e24735137b4f3
okbeng03 <okbeng03@live.com>
2022-02-08 15:19:26 +0800

    chore: semver

--- packages/nidle-web/config/config.prod.js
+++ packages/nidle-web/config/config.prod.js
@@ -40,40 +40,6 @@ exports.logger = {
   dir: '/frontend/nidle-output/nidle-web'
 }
 
-// exports.nidle = {
-//   output: {
-//     backup: {
-//       path: '/Users/wangchangbin/git/nidle-output/backup/'
-//     },
-//     path: '/Users/wangchangbin/git/nidle-output/output/'
-//   },
-//   source: '/Users/wangchangbin/git/nidle-output/source/',
-//   log: {
-//     path: '/Users/wangchangbin/git/nidle-output/logs/'
-//   },
-//   config: {
-//     path: '/Users/wangchangbin/git/nidle-output/config/'
-//   },
-//   environments: [
-//     {
-//       value: 'development',
-//       label: '测试'
-//     },
-//     {
-//       value: 'pre',
-//       label: '预发布'
-//     },
-//     {
-//       value: 'production',
-//       label: '生产'
-//     }
-//   ]
-// }
-
-// exports.logger = {
-//   dir: '/Users/wangchangbin/git/nidle-output/nidle-web'
-// }
-
 // exports.mailer = {
 //   host: "smtp.ethereal.email",
 //   port: 587,
