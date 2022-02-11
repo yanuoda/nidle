afd511ba678c3e08259880c88cee14f3d60a2d15
okbeng03 <okbeng03@live.com>
2022-02-11 15:30:37 +0800

    fix: 应用配置没有执行chain

--- packages/nidle-web/app/controller/config.js
+++ packages/nidle-web/app/controller/config.js
@@ -1,9 +1,9 @@
 'use strict'
 
 // 配置信息
-
+const NildeChain = require('nidle-chain')
+const _ = require('lodash')
 const Controller = require('../core/base_controller')
-
 class ConfigController extends Controller {
   /**
    * 获取应用对应环境配置 #swagger-api
@@ -19,9 +19,21 @@ class ConfigController extends Controller {
     const { ctx } = this
 
     try {
-      const result = await ctx.service.config.getByApp(ctx.request.body)
+      const config = await ctx.service.config.getByApp(ctx.request.body)
 
-      this.success(result)
+      // 有chain，处理chain
+      if (config.chain && _.isFunction(config.chain)) {
+        const chainFun = config.chain
+        delete config.chain
+
+        const newConfig = new NildeChain()
+        newConfig.merge(config)
+        chainFun(newConfig)
+        this.success(newConfig.toConfig())
+        return
+      }
+
+      this.success(config)
     } catch (err) {
       this.failed({
         msg: err.message
