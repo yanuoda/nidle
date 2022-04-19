17a3a115c65df78090d450f8b7f9cbe678ad3ef7
hanrenguang <413526182@qq.com>
2022-04-19 17:29:45 +0800

    fix: nidle-cli 更新版本时，先对依赖进行 merge 再 diff

--- packages/nidle-cli/src/commands/utils/validateIfDepsUpdate.js
+++ packages/nidle-cli/src/commands/utils/validateIfDepsUpdate.js
@@ -1,21 +1,43 @@
+const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
 
-function diff(oldDeps, newDeps) {
+function rewriteDeps(filePath, packageInfo, depsKey, val) {
+  packageInfo[depsKey] = val
+  fs.writeFileSync(filePath, JSON.stringify(packageInfo, null, 2))
+}
+
+function mergeAndDiff(oldDeps, newDeps) {
+  const mergeDepsMap = JSON.parse(JSON.stringify(newDeps))
+  const retainPlugins = []
   const oldDepsList = Object.entries(oldDeps)
-  const newDepsList = Object.entries(newDeps)
-
-  if (oldDepsList.length !== newDepsList.length) {
-    return true
-  }
+  let isDiff = false
 
   for (const [oldDepKey, oldDepVal] of oldDepsList) {
     const newDepVal = newDeps[oldDepKey]
-    if (!newDepVal || newDepVal !== oldDepVal) {
-      return true
+    if (!newDepVal && /^nidle-plugin/.test(oldDepKey)) {
+      // 旧版项目存在不同于新项目的插件依赖，对于插件之外的依赖将不再保留
+      isDiff = true
+      retainPlugins.push(`${oldDepKey}@${oldDepVal}`)
+      mergeDepsMap[oldDepKey] = oldDepVal
+    } else if (newDepVal && newDepVal !== oldDepVal) {
+      // 新旧依赖版本不同，以新版依赖为准
+      isDiff = true
     }
   }
 
-  return false
+  if (retainPlugins.length > 0) {
+    console.log(
+      chalk.yellow(
+        `
+  nidle-web 旧版依赖的以下插件将被保留，如需升级插件版本，请更新完成后自行升级：\n${retainPlugins
+    .map(plugin => `    - ${plugin}`)
+    .join('\n')}\n`
+      )
+    )
+  }
+
+  return { isDiff, mergeDepsMap }
 }
 
 /**
@@ -25,11 +47,16 @@ function diff(oldDeps, newDeps) {
  * @returns deps 是否有更新
  */
 function validateIfDepsUpdate(oldDir, tempDir) {
-  const { dependencies: oldDeps } = require(path.resolve(oldDir, 'package.json'))
-  const { dependencies: newDeps } = require(path.resolve(tempDir, 'package.json'))
+  const oldPackageInfo = require(path.resolve(oldDir, 'package.json'))
+  const newPackageInfo = require(path.resolve(tempDir, 'package.json'))
 
   // diff
-  return diff(oldDeps, newDeps)
+  const { isDiff, mergeDepsMap } = mergeAndDiff(oldPackageInfo.dependencies, newPackageInfo.dependencies)
+  if (isDiff) {
+    // 有变化
+    rewriteDeps(path.resolve(tempDir, 'package.json'), newPackageInfo, 'dependencies', mergeDepsMap)
+  }
+  return isDiff
 }
 
 /**
@@ -39,11 +66,16 @@ function validateIfDepsUpdate(oldDir, tempDir) {
  * @returns deps 是否有更新
  */
 function validateIfDevDepsUpdate(oldDir, tempDir) {
-  const { devDependencies: oldDeps } = require(path.resolve(oldDir, 'package.json'))
-  const { devDependencies: newDeps } = require(path.resolve(tempDir, 'package.json'))
+  const oldPackageInfo = require(path.resolve(oldDir, 'package.json'))
+  const newPackageInfo = require(path.resolve(tempDir, 'package.json'))
 
   // diff
-  return diff(oldDeps, newDeps)
+  const { isDiff, mergeDepsMap } = mergeAndDiff(oldPackageInfo.devDependencies, newPackageInfo.devDependencies)
+  if (isDiff) {
+    // 有变化
+    rewriteDeps(path.resolve(tempDir, 'package.json'), newPackageInfo, 'devDependencies', mergeDepsMap)
+  }
+  return isDiff
 }
 
 module.exports = {
