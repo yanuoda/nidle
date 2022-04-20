1bca6028339ba0bc726e27b9627c3392335fe8d4
hanrenguang <413526182@qq.com>
2022-04-20 14:14:00 +0800

    fix: cp 命令添加 -f 选项

--- packages/nidle-cli/src/commands/steps/coverNidleFiles.js
+++ packages/nidle-cli/src/commands/steps/coverNidleFiles.js
@@ -2,10 +2,10 @@ const path = require('path')
 const { Logger, runCommand } = require('../utils')
 
 module.exports = async function coverNidleFiles(oldPath, tempPath) {
-  await runCommand('cp', [path.resolve(tempPath, './nidle-spa'), oldPath, '-r']).catch(err => {
+  await runCommand('cp', [path.resolve(tempPath, './nidle-spa'), oldPath, '-rf']).catch(err => {
     new Logger('nidle-spa 更新文件文件拷贝').error(`nidle-spa 更新文件拷贝失败，请重试！\n${err}`)
   })
-  await runCommand('cp', [path.resolve(tempPath, './nidle-web'), oldPath, '-r']).catch(err => {
+  await runCommand('cp', [path.resolve(tempPath, './nidle-web'), oldPath, '-rf']).catch(err => {
     new Logger('nidle-web 更新文件文件拷贝').error(`nidle-web 更新文件拷贝失败，请重试！\n${err}`)
   })
 }
