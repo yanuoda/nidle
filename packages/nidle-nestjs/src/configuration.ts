8ced21330c1ee9d88bdb48f6ebc1086b955b96de
BarneyZhao <zxzswa@163.com>
2023-07-13 17:58:35 +0800

    build: config补全

--- packages/nidle-nestjs/src/configuration.ts
+++ packages/nidle-nestjs/src/configuration.ts
@@ -11,4 +11,28 @@ export const dbConfig = registerAs('dbConfig', () => ({
   database: process.env.DB_BASE,
 }));
 
-export default [dbConfig];
+export const redisConfig = registerAs('redisConfig', () => ({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  pass: process.env.REDIS_PASSWORD,
+  dbIndex: process.env.REDIS_DB_INDEX,
+}));
+
+export const nidleConfig = registerAs('nidleConfig', () => ({
+  output: {
+    backup: {
+      path: process.env.NIDLE_DIR + '/nidle-output/backup/',
+    },
+    path: process.env.NIDLE_DIR + '/nidle-output/output/',
+  },
+  source: process.env.NIDLE_DIR + '/nidle-output/source/',
+  log: {
+    path: process.env.NIDLE_DIR + '/nidle-output/logs/',
+  },
+  config: {
+    path: process.env.NIDLE_DIR + '/nidle-output/config/',
+  },
+  afterManagerWaitSecs: process.env.AFTER_MANAGER_WAIT_SECS,
+}));
+
+export default [dbConfig, redisConfig, nidleConfig];
