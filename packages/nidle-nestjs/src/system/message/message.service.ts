9310d0fc6627b521963c06200fc0dfd1eccd62e1
okbeng03 <okbeng03@live.com>
2023-08-22 16:24:45 +0800

    fix: 移除已关闭的sse

--- packages/nidle-nestjs/src/system/message/message.service.ts
+++ packages/nidle-nestjs/src/system/message/message.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, MessageEvent as CommonMessageEvent } from '@nestjs/common';
 import { Observable, Subscriber } from 'rxjs';
 import { MessageEvent } from './message.interface';
-import * as EventEmitter from 'events';
+import { remove } from 'lodash';
+// import * as EventEmitter from 'events';
 
 @Injectable()
 export class MessageService {
-  private readonly emitter = new EventEmitter();
+  // private readonly emitter = new EventEmitter();
+  private sseSubscribers = [];
 
   send(message: MessageEvent) {
     this.sendSse(message);
@@ -13,13 +15,21 @@ export class MessageService {
 
   createSse() {
     return new Observable((subscriber: Subscriber<CommonMessageEvent>) => {
-      this.emitter.on('send', (data: any) => {
-        subscriber.next({ data });
-      });
+      // this.emitter.on('send', (data: any) => {
+      //   subscriber.next({ data });
+      // });
+      remove(this.sseSubscribers, (subscriber) => subscriber.closed);
+      this.sseSubscribers.push(subscriber);
     });
   }
 
   sendSse(message: MessageEvent) {
-    this.emitter.emit('send', message);
+    // this.emitter.emit('send', message);
+    remove(this.sseSubscribers, (subscriber) => subscriber.closed);
+    this.sseSubscribers.forEach((subscriber) => {
+      subscriber.next({
+        data: message,
+      });
+    });
   }
 }
