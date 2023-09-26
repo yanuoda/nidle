77f6602a047f64cad46ce05fbc784cdca6fc460f
BarneyZhao <zxzswa@163.com>
2023-09-26 10:12:11 +0800

    feat: spa - 首页增加快捷入口

--- packages/nidle-spa/src/pages/Welcome.jsx
+++ packages/nidle-spa/src/pages/Welcome.jsx
@@ -1,15 +1,31 @@
 import { useState, useEffect } from 'react'
 import { PageContainer } from '@ant-design/pro-layout'
-import { Card, Radio, notification } from 'antd'
+import { Card, Radio, Button, notification } from 'antd'
+import { Link } from 'umi'
 
-import { NOTIFICATION_SETTING_KEY } from '@/config'
+import { NOTIFICATION_SETTING_KEY, OFFEN_USE_PROJECTS_KEY } from '@/config'
 
 export default function Welcome() {
   const [notificationSetting, setnotificationSetting] = useState(
     JSON.parse(localStorage.getItem(NOTIFICATION_SETTING_KEY) || '{}')
   )
+  const offenUseProjects = JSON.parse(localStorage.getItem(OFFEN_USE_PROJECTS_KEY) || '[]')
+
   const setnotificationMode = val => {
     setnotificationSetting(_setting => ({ ..._setting, mode: val }))
+  }
+
+  const renderShortcut = () => {
+    if (!offenUseProjects.length) {
+      return '暂无'
+    }
+    return offenUseProjects.map(({ id, name }) => (
+      <Link key={id} to={`/project/publish?id=${id}&name=${name}`}>
+        <Button key={id} type="link">
+          {name}
+        </Button>
+      </Link>
+    ))
   }
 
   useEffect(() => {
@@ -30,6 +46,9 @@ export default function Welcome() {
           onChange={e => setnotificationMode(e.target.value)}
           value={notificationSetting.mode || 'all'}
         />
+      </Card>
+      <Card title="快捷入口" style={{ marginTop: 15 }}>
+        {renderShortcut()}
       </Card>
     </PageContainer>
   )
